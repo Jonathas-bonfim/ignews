e592abd0e2d76feb50601e2e6e514f7917d10e5d
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-05-10 20:53:27 -0300

    Style Global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <>
-      <title>Home | ig.news</title>
-      <h1 className={styles.title}>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1 >
         Hello World
       </h1>
     </>
