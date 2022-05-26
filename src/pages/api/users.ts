8f36fe150439ef7b3c404d2bc044aac29600ddb2
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-05-26 20:33:56 -0300

    Static Site Generation (SSG)

---
+++ src/pages/api/users.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: 'Jonathas 1' },
+    { id: 2, name: 'Jonathas 2' },
+    { id: 3, name: 'Jonathas 3' },
+  ]
+
+  return response.json(users)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './styles.module.scss';
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { stripe } from '../services/stripe';
 
 interface HomeProps {
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1L37uZL43ABWKV8HB5EBvmxA", {
     expand: ['product']
   })
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }
