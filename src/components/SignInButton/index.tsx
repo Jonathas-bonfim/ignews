80dc08b3c4daab46bce7443e7da4ef644eb4dd4b
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-05-26 22:05:54 -0300

    Autenticação com Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,30 @@
 import styles from './styles.module.scss';
 
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session);
+
+  return session ? (
     <button
+      type="button"
       className={styles.signInButton}
-      type="button">
+      onClick={() => signOut()}
+    >
       <FaGithub color='#04d361' />
-      Jônathas Bonfim
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
     <button
+      type="button"
       className={styles.signInButton}
-      type="button">
+      onClick={() => signIn('github')}
+    >
       <FaGithub color='#eba417' />
       Sing in with GitHub
     </button>
