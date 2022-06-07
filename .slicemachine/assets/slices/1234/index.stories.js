70d841e30b234a1470f42b6b5cbfb1eef49872f7
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-06-07 09:55:05 -0300

    Configure Prismic CMS

---
+++ .slicemachine/assets/slices/1234/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/1234';
+
+export default {
+  title: 'slices/1234'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"1234","items":[],"primary":{"title":[{"type":"heading1","text":"Unleash plug-and-play niches","spans":[]}],"description":[{"type":"paragraph","text":"Reprehenderit do duis enim occaecat labore ea excepteur ipsum labore culpa proident.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
