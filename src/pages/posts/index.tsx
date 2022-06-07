ecfd0574e71d9c750953d1e4f4bc81b10384f567
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-06-07 10:19:35 -0300

    styles posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Teste de título de publicação bla bla bla</strong>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </a>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Teste de título de publicação bla bla bla</strong>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </a>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Teste de título de publicação bla bla bla</strong>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
